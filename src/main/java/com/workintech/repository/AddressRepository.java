d5ac7f7101aee43948e60b19bf7b97263c6af61a
Alişan <alisankarababa@gmail.com>
2023-12-24 19:05:36 +0300

    add repositories

---
+++ src/main/java/com/workintech/repository/AddressRepository.java
@@ -0,0 +1,7 @@
+package com.workintech.repository;
+
+import com.workintech.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
